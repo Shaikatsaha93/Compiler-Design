8d567eb7ffc77fa1c4525db4e7de2fe27cfad9e5
Shaikat Saha <32926005+Shaikatsaha93@users.noreply.github.com>
2017-11-21 11:45:25 +0600

    Add files via upload

---
+++ P11.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p11;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DIU
+ */
+public class P11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a , i ,j; 
+      Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of rows");
+    a = sc.nextInt();
+ 
+       for(i=1; i<=a;i++){
+            for(j=a; j>=i;j--){
+                System.out.print(" ");
+            }
+            if(i==1){
+                for(j=1; j<=(i*2)-1;j++){
+                    System.out.print("*");
+                }
+            }else{
+                System.out.print("*");
+                for(j=1; j<(i*2)-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println("");
+        }  
+       for(i=a-1; i>=1;i--){
+            for(j=i; j<=a;j++){
+                System.out.print(" ");
+            }
+            if(i==1){
+                for(j=1; j<=(i*2)-1;j++){
+                    System.out.print("*");
+                }
+            }else{
+                System.out.print("*");
+                for(j=1; j<(i*2)-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println("");
+        }    
+
+    }
+    
+}
